3e304470287e1211124ad374ca426bccf835f62c
yu shimamura <mashiyu0914@gmail.com>
2020-05-25 07:18:44 +0900

    add: admin user 一通り

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
+
+  before_validation :set_nameless_name
+
   validates :name,
     presence: true,
     length: {maximum: 30}
@@ -7,6 +10,10 @@ class Task < ApplicationRecord
   private
     def validate_name_not_including_comma
       errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+    end
+
+    def set_nameless_name
+      self.name = '名前なし' if name.blank?
     end
 
 end
