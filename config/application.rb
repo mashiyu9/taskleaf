264d40bfa6db9076df0833dd1d1150282303539c
yu shimamura <mashiyu0914@gmail.com>
2020-05-25 00:06:12 +0900

    add: 日本語化ファイルの作成

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,13 @@ Bundler.require(*Rails.groups)
 module Taskleaf
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
